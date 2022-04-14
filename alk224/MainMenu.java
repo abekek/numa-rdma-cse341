7ef1b633e5b754e0fa64a0e03f9b2f8bb4f4e941
Alibek Kaliyev <alk224@nereid.cse.lehigh.edu>
2022-04-14 12:00:57 -0400

    some improvements of the main menu

--- alk224/MainMenu.java
+++ alk224/MainMenu.java
@@ -6,21 +6,24 @@ import java.util.*;
 public class MainMenu{
     public static void main(String[] args) throws SQLException, IOException, java.lang.ClassNotFoundException{
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Welcome to Northside Uncommons Management of Apartments (NUMA)!");
-        System.out.println("Before using our system, you have to login first.");
+        System.out.println("------------------------------------------------------");
+        System.out.println("Welcome to Northside Uncommons Management of Apartments (NUMA)!\n");
+        System.out.println("Before using our system, you have to login first.\n");
         System.out.println("Please choose whether you want to login as a tenant (or prospective) or a manager.");
 
         int choice;
 
         do{
-            System.out.println("1. Tenant");
-            System.out.println("2. Manager");
-            System.out.println("3. Exit");
+            System.out.println("1. Property Manager");
+            System.out.println("2. Tenant");
+            System.out.println("3. NUMA Manager");
+            System.out.println("4. Business Manager");
+            System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
             
             // validating entered year
             if (!scnr.hasNextInt()){
-                System.out.println("Please input an integer.");
+                System.out.println("Please input an integer.\n");
                 scnr.next();
                 continue;
             } else {
@@ -30,13 +33,16 @@ public class MainMenu{
             switch(choice){
                 case 1:
                 case 2:
-                    break;
                 case 3:
+                case 4:
+                    break;
+                case 5:
                     System.out.println("Thank you for using NUMA!");
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("Invalid choice. Please try again.\n");
+                    continue;
             }
             break;
         } while(true);
@@ -57,16 +63,19 @@ public class MainMenu{
                 Connection con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241", userID, password);
                 Statement s = con.createStatement();
             ){
+                isSuccessfullLogin = true;
                 switch(choice){
                     case 1:
-                        // TenantMenu.tenantMenu(con, s, userID);
-                        System.out.println("Tenant menu is not implemented yet.");
-                        isSuccessfullLogin = true;
+                        System.out.println("Property Manager menu is not implemented yet.");
                         break;
                     case 2:
-                        // ManagerMenu.managerMenu(con, s, userID);
-                        System.out.println("Manager menu is not implemented yet.");
-                        isSuccessfullLogin = true;
+                        System.out.println("Tenant menu is not implemented yet.");
+                        break;
+                    case 3:
+                        System.out.println("NUMA Manager menu is not implemented yet.");
+                        break;
+                    case 4:
+                        System.out.println("Business Manager menu is not implemented yet.");
                         break;
                 }
             } catch(SQLException e){
